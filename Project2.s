90975ba9013cffac0d6cf63f48a2ff19feb6c3eb
Nedir-Castelani <136397570+IratedeviationBR@users.noreply.github.com>
2024-12-20 20:59:32 -0300

    Add files via upload

---
+++ Project2.s
@@ -0,0 +1,19 @@
+#        Compilação Usando Variavel para indexar array
+#
+# Nedir Castelani 20/dez/24
+# 
+# A[100] # array 
+#
+#       $s1 = $s2 + A[$s4]
+
+.global _start
+_start:
+	
+	add $t1, $s4, $s4	# $s4 * 2
+	add $t1, $t1, $t1	# ($s4 * 2)+($s4 * 2)
+	add $t1, $t1, $s3	# $t1 recebe endereço de A[i]
+	lw $t0, 0($t1)   	# $t0 recebe A[i]
+	add $s1, $s2, $t0	# $s1 recebe A[i] = $s2
+	
+main:
+	
