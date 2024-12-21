8653a486ee2b4732d8af66d0b2fc146214a83031
Nedir-Castelani <136397570+IratedeviationBR@users.noreply.github.com>
2024-12-20 21:00:25 -0300

    Update Project2.s

--- Project2.s
+++ Project2.s
@@ -10,7 +10,7 @@
 _start:
 	
 	add $t1, $s4, $s4	# $s4 * 2
-	add $t1, $t1, $t1	# ($s4 * 2)+($s4 * 2)
+	add $t1, $t1, $t1	# ($s4 * 2)+($s4 * 2)  --> $s4 * 4
 	add $t1, $t1, $s3	# $t1 recebe endereço de A[i]
 	lw $t0, 0($t1)   	# $t0 recebe A[i]
 	add $s1, $s2, $t0	# $s1 recebe A[i] = $s2
